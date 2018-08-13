23cd6da89e3bfc970429b56df4f574d824132443
Aiton Goldman <agoldma@us.ibm.com>
2018-08-13 07:32:48 -0400

    pair down for part 1

--- lib/auth/permissions.py
+++
@@ -1,9 +0,0 @@
-from flask_principal import Permission
-from lib.auth import needs
-
-class EventCreatorPermission(Permission):
-    def __init__(self,event_id=None):
-        login_need = needs.EventCreatorRoleNeed()
-        super(EventCreatorPermission, self).__init__(login_need)
-
-                 
--- proxies/EventRolesProxy.py
+++
@@ -1,9 +0,0 @@
-from models.EventRoles import generate_event_roles_model
-
-class EventRolesProxy():
-    def __init__(self,sqlAlchemyHandle):                
-        self.sqlAlchemyHandle = sqlAlchemyHandle
-        self.event_roles_model = generate_event_roles_model(self.sqlAlchemyHandle)
-        
-    def get_event_roles(self):        
-        pass        
