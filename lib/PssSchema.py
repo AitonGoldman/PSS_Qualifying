6c7d6532953ce7f60c632633fc9e7a354e384367
Aiton Goldman <agoldma@us.ibm.com>
2018-09-01 12:32:02 -0400

    added unit tests for PssSchema

--- lib/PssSchema.py
+++ lib/PssSchema.py
@@ -1,34 +1,36 @@
-from marshmallow import fields
+from marshmallow import fields as marshmallow_fields
 from proxies import TableProxyError
+import inspect
 
 class PssSchemaBuilder(object):    
     
-    def __init__(self,app,model_name,model,exclude=None,session=None):
+    def __init__(self,app,model_name,model,exclude=None,session=None,test_marshmallow_fields=None):
         self.app = app                        
         self.model_name = model_name
         self.model = model
         self.exclude = exclude
-        self.session = session        
+        self.session = session
+        self.marshmallow_fields = test_marshmallow_fields if test_marshmallow_fields else marshmallow_fields
 
     def initialize_pss_schema(self):
         self.schema = self.build_schema(self.model)
         return self
     
     def build_schema(self,model,depth=0):        
-        relationships = {}        
-        for relationship in model.__mapper__.relationships:                         
+        relationships = {}                
+        for relationship in model.__mapper__.relationships:                                     
             new_schema = self.build_schema(model=relationship.mapper.class_,depth=depth+1)            
-            relationships[relationship.key]=fields.Nested(new_schema,many=relationship.uselist)            
+            relationships[relationship.key]=self.marshmallow_fields.Nested(new_schema,many=relationship.uselist)            
         meta_params = {'model':model,'include_fk':True}
         if self.exclude:
             meta_params['exclude']=self.exclude
         if self.session:
             meta_params['sqla_session']=self.session
         meta_class = type("Meta",(object,),meta_params)
-        schema_properties = {'Meta':meta_class}
+        schema_properties = {'Meta':meta_class}        
         for key,relationship in relationships.items():
-            schema_properties[key]=relationship
-        schema = type(self.model_name+"Schema",(self.app.ma.ModelSchema,),schema_properties)        
+            schema_properties[key]=relationship        
+        schema = type(self.model_name+"Schema%s"%depth,(self.app.ma.ModelSchema,),schema_properties)        
         return schema
             
     def add_sqlAlchemy_relationship_to_schema(self,field_name,nested_schema,many=True):
--- tests/unit/test_pss_schema_lib.py
+++ tests/unit/test_pss_schema_lib.py
@@ -1,10 +1,40 @@
 import unittest
 from mock import MagicMock
 from pss_unit_test_base import PssUnitTestBase
+from lib.PssSchema import PssSchemaBuilder
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields as marshmallow_fields
 
 class PssSchemaLibTest(PssUnitTestBase):
     def setUp(self):
         pass
+    def initialize_mock_nested_relationship(self):
+        mock_relationship = MagicMock()
+        mock_relationship.key="mock_relationship"
+        mock_relationship.uselist=False        
+        mock_relationship.mapper.class_.__mapper__=MagicMock()        
+        return mock_relationship
     
     def test_pss_schema(self):                        
-        pass
+        mock_app = MagicMock()
+        mock_app.ma.ModelSchema = ModelSchema
+        model_name = "test_model"
+        mock_model = MagicMock()
+        mock_model.__mapper__=MagicMock()
+        mock_session = MagicMock()
+        mock_marshmallow_fields = MagicMock()
+        mock_marshmallow_fields.Nested = marshmallow_fields.Nested        
+        mock_relationship = self.initialize_mock_nested_relationship()
+        mock_model.__mapper__.relationships=[mock_relationship]
+        
+        
+        pss_schema_builder = PssSchemaBuilder(mock_app,
+                                              model_name,
+                                              mock_model,
+                                              session=mock_session,
+                                              test_marshmallow_fields=mock_marshmallow_fields)
+
+        test_schema = pss_schema_builder.initialize_pss_schema()        
+        self.assertEqual(test_schema.schema.__name__,"test_modelSchema0")                
+        self.assertEqual(type(test_schema.schema._declared_fields.get('mock_relationship',None)),marshmallow_fields.Nested)
+                
